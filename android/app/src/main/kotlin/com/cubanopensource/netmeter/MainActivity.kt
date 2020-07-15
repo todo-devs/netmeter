aee556f8a887462d9c5636b2656371b0794cf89d
Luis Correa <correaleyval@gmail.com>
2020-07-15 17:59:37 -0400

    Traffic Stats Service

--- android/app/src/main/kotlin/com/cubanopensource/netmeter/MainActivity.kt
+++ android/app/src/main/kotlin/com/cubanopensource/netmeter/MainActivity.kt
@@ -1,13 +1,9 @@
 package com.cubanopensource.netmeter
 
-import android.content.Context
 import android.content.Intent
-import android.graphics.PixelFormat
 import android.net.Uri
 import android.os.Build
 import android.provider.Settings
-import android.util.DisplayMetrics
-import android.view.*
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
@@ -25,7 +21,7 @@ class MainActivity : FlutterActivity() {
                 call.method == "getPlatformVersion" -> result.success("Android ${Build.VERSION.RELEASE}")
                 call.method == "getDrawPermissionState" -> result.success(getDrawPermissionState())
                 call.method == "reqDrawPermission" -> result.success(reqDrawPermission())
-                call.method == "showWidget" -> showFloatWidget()
+                call.method == "showWidget" -> startService(Intent(this, FloatingWindow::class.java))
                 else -> result.notImplemented()
             }
 
@@ -43,114 +39,6 @@ class MainActivity : FlutterActivity() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:${packageName}"))
             startActivityForResult(intent, 1)
-        }
-    }
-
-    private fun showFloatWidget() {
-        if (getDrawPermissionState()) {
-            val displayMetrics = DisplayMetrics()
-            windowManager.defaultDisplay.getMetrics(displayMetrics)
-            val height = displayMetrics.heightPixels
-            val width = displayMetrics.widthPixels
-
-            // Close traffic stats widget
-            val closeView = LayoutInflater.from(this).inflate(R.layout.close_float_widget, null)
-
-            val closeWM = getSystemService(Context.WINDOW_SERVICE) as WindowManager
-
-            val parameters_close: WindowManager.LayoutParams
-
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
-                parameters_close = WindowManager.LayoutParams(
-                        width,
-                        WindowManager.LayoutParams.WRAP_CONTENT,
-                        WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
-                        WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-                        PixelFormat.TRANSLUCENT
-                )
-            else
-                parameters_close = WindowManager.LayoutParams(
-                        width,
-                        WindowManager.LayoutParams.WRAP_CONTENT,
-                        WindowManager.LayoutParams.TYPE_PHONE,
-                        WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-                        PixelFormat.TRANSLUCENT
-                )
-
-            parameters_close.gravity = Gravity.BOTTOM
-
-            // Traffic Stats widget
-            val widgetView = LayoutInflater.from(this).inflate(R.layout.float_window, null)
-
-            val wm = getSystemService(Context.WINDOW_SERVICE) as WindowManager
-
-            val parameters: WindowManager.LayoutParams
-
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
-                parameters = WindowManager.LayoutParams(
-                        WindowManager.LayoutParams.WRAP_CONTENT,
-                        WindowManager.LayoutParams.WRAP_CONTENT,
-                        WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
-                        WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-                        PixelFormat.TRANSLUCENT
-                )
-            else
-                parameters = WindowManager.LayoutParams(
-                        WindowManager.LayoutParams.WRAP_CONTENT,
-                        WindowManager.LayoutParams.WRAP_CONTENT,
-                        WindowManager.LayoutParams.TYPE_PHONE,
-                        WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-                        PixelFormat.TRANSLUCENT
-                )
-
-            parameters.x = 0
-            parameters.y = 100
-            parameters.gravity = Gravity.CENTER
-            wm.addView(widgetView, parameters)
-
-            // Traffic stats on touch handler
-            widgetView.setOnTouchListener(object : View.OnTouchListener {
-                private var updatedParameters: WindowManager.LayoutParams = parameters
-                var x: Int = 0
-                var y: Int = 0
-
-                var touchedX: Float = 0.0f
-                var touchedY: Float = 0.0f
-
-                override fun onTouch(v: View?, event: MotionEvent?): Boolean {
-                    when (event?.action) {
-                        MotionEvent.ACTION_DOWN -> {
-
-                            x = updatedParameters.x
-                            y = updatedParameters.y
-
-                            touchedX = event.rawX
-                            touchedY = event.rawY
-
-                            closeWM.addView(closeView, parameters_close)
-                        }
-                        MotionEvent.ACTION_MOVE -> {
-                            updatedParameters.x = (x + (event.rawX - touchedX)).toInt()
-                            updatedParameters.y = (y + (event.rawY - touchedY)).toInt()
-
-                            wm.updateViewLayout(widgetView, updatedParameters)
-                        }
-
-                        MotionEvent.ACTION_UP -> {
-                            if (event.rawY.toInt() >= height - 100) {
-                                println("POR FIN")
-                                wm.removeView(widgetView)
-                            }
-
-                            closeWM.removeView(closeView)
-                        }
-                    }
-
-                    return false
-                }
-            })
-
-
         }
     }
 }
